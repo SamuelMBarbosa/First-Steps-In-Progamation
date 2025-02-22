cfc712d06b06ba1ca87ada00c62ff8fcd9d47c9c
Samuel M. Barbosa <samuelmmbarbosa18@gmail.com>
2025-02-22 15:08:52 -0300

    Add files via upload

--- src/pctHórarioCursinho/clsHorárioCursinho.java
+++ src/pctHórarioCursinho/clsHorárioCursinho.java
@@ -1,7 +1,7 @@
 package pctHórarioCursinho;
 
 import java.util.Scanner;
-
+//futuramente implementar uma interface nesse código, de modo que funione no mobile//
 public class clsHorárioCursinho {
 
 	public static void main(String[] args){
@@ -9,48 +9,28 @@ public class clsHorárioCursinho {
 			Scanner sc = new Scanner(System.in);		
 		        String seg, ter, qua, qui, sex;
 		        seg = "\nMatutino:\nBiologia (Jaqueline)\nFísica (Eduardo)\nIntervalo\nGeografia (França)\nFísica (Eduardo)\nIntervalo\nMatemática (Valtencir)\nGeografia (França)\n\nVespertino:\nArte (Pedro)\nHistória (Gibran)\nFilosofia (Pedro)\nIntervalo\nQuímica (Henrique)\nMatemática (Valtencir)\n";
-		        // não mexe nessa linha senão vai dar merda //
 		        ter = "\nMatutino:\nBiologia (Kamilla)\nFísica (Humberto)\nIntervalo\nFísica (Humberto)\nBiologia (Kamilla)\nIntervalo\nMatemática (Valtencir)\nPort/Red (Maroja)\n\nVespertino:\nInglês/Espanhol (Silvana)\nGeografia (França)\nHistória (Gibran)\nIntervalo\nQuímica (Henrique)\nGeografia (França)\\n";
 		        qua = "\nMatutino:\nBiologia (Jaqueline)\nPort/Red (Maroja)\nIntervalo\nHistória (Gibran)\nBiologia (Jaqueline)\nIntervalo\nHistória (Gibran)\nQuímica (Henrique)\n\nVespertino:\nPlantão de dúvidas\\n";
 		        qui = "\nMatutino:\nPort/Red (Maroja)\nQuímica (Henrique)\nIntervalo\nMatemática (Valtencir)\nFísica (Humberto)\nIntervalo\nPort/Red (Maroja)\nQuímica (Henrique)\n\nVespertino:\nMat. Básica/Atualidades\nMat. Básica/Atualidades\nAula Variável\nIntervalo\nAula Variável\\n";
 		        sex = "\nMatutino:\nBiologia (Kamilla)\nSociologia (Gibran)\nIntervalo\nLiteratura (Mariana)\nMatemática (Valtencir)\nIntervalo\nMatemática (Valtencir)\nLiteratura (Mariana)\n\nVespertino:\nSimulados\n";
-		        
+		        //tenho que achar algum jeito de minimizar esse texto todo, aprender como criar um array de dados//
 		        System.out.println("Olá! Você quer saber o horário de qual dia de curso?");
-		        String resposta = sc.nextLine();
-		        
+		        String resposta = sc.nextLine().toLowerCase();
+		        //quando eu tiver mais avançado, procurar implementar o try map//
 		        switch(resposta){
-		            case "Segunda":
-		            System.out.println(seg);
-		            break;
 		            case "segunda":
 		            System.out.println(seg);
 		            break;
-		            case "Terça":
-		            System.out.println(ter);
-		            break;
-		            case "Terca":
-		            System.out.println(ter);
-		            break;
 		            case "terça":
 		            System.out.println(ter);
-		            break;
 		            case "terca":
 		            System.out.println(ter);
-		            break;
-		            case "Quarta":
-		            System.out.println(qua);
 		            break;
 		            case "quarta":
 		            System.out.println(qua);
 		            break;
-		            case "Quinta":
-		            System.out.println(qui);
-		            break;
 		            case "quinta":
 		            System.out.println(qui);
-		            break;
-		            case "Sexta":
-		            System.out.println(sex);
 		            break;
 		            case "sexta":
 		            System.out.println(sex);
